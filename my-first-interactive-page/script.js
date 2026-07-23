22a455ea4d666bd2e08f1e7d560dd1e61532fb8f
Brandon De Wet <brandond@redpandasoftware.co.za>
2026-07-23 10:26:57 +0200

    folder cleanup and correct PM values

--- my-first-interactive-page/script.js
+++
@@ -1,8 +0,0 @@
-console.log("JavaScript file loaded.");
-
-//The welcomeBtn is the ID given to the button in HTML
-const button = document.getElementById("welcomeBtn");
-
-button.addEventListener("click", function() {
-  alert("Welcome to my First JavaScript page!")
-});
--- my-first-js-project/script.js
+++
@@ -1,7 +0,0 @@
-//This message helps us confirm that the file loaded.
-console.log("The JavaScript file loaded correctly.");
-
-//This function runs when the button is clicked.
-function submitRequest() {
-	alert("Thank You. Your Request has been received.");
-}
