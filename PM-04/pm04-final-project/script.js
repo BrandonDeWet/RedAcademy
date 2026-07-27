eb4cdf8eccdbe5c8f06d71afa5ec72f5e76102c4
Brandon De Wet <brandond@redpandasoftware.co.za>
2026-07-27 16:37:40 +0200

    Added function to JS to display the requester name, and also had to fix a issue with a variable that was undefined. 'priority' to 'urgnecy'

--- PM-04/pm04-final-project/script.js
+++ PM-04/pm04-final-project/script.js
@@ -1,13 +1,13 @@
 // Adding an array
 let requests = [];
 
-// Adding const variables based off our HTML page Note, The ID's are based off the id's for the labels in the HTML
+// Adding const variables based off our HTML page Note, The ID's are based off the id's in the HTML
 const form = document.getElementById("requestForm");
 const requesterName = document.getElementById("requesterName");
 const department = document.getElementById("department");
 const resourceType = document.getElementById("resourceType");
 const details = document.getElementById("details");
-const urgency = document.getElementById("priority");
+const urgency = document.getElementById("urgency");
 
 // Function for adding an event listener to our form
 form.addEventListener("submit",function(event){
@@ -19,7 +19,7 @@ form.addEventListener("submit",function(event){
     department: department.value,
     resource: resourceType.value,
     details: details.value,
-    priority: priority.value
+    priority: urgency.value
   };
   // Pushes data into the array for the request variable above
   requests.push(request);
@@ -27,6 +27,14 @@ form.addEventListener("submit",function(event){
 
 // Logs out to the Console when JS gets loaded via the HTML page
 console.log("JavaScript file loaded.");
+
+// Add function to display information gathered from the form
+// When adding a string in JS, do not use ' but rather use ` else you will get code errors
+function displaySummary(request){
+  summaryContent.innerHTML = `
+  <p><strong>Requester:</strong> ${request.requester}</p>
+  `;
+}
 
 //The requestSend is the ID given to the button in HTML
 const button = document.getElementById("requestSend");
