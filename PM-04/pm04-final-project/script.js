20f9eedca5917b10614d2f1d83b48f45a9da7a0c
Brandon De Wet <brandond@redpandasoftware.co.za>
2026-07-27 14:23:55 +0200

    Updated JS file to be linked to the submit button id, and added additonal console logging, also added a word doc for evidence of testing

--- PM-04/pm04-final-project/script.js
+++ PM-04/pm04-final-project/script.js
@@ -1,9 +1,11 @@
+// Logs out to the Console when JS gets loaded via the HTML page
 console.log("JavaScript file loaded.");
 
 //The welcomeBtn is the ID given to the button in HTML
-const button = document.getElementById("welcomeBtn");
+const button = document.getElementById("requestSend");
 
 button.addEventListener("click", function() {
-  alert("Welcome to my First JavaScript page!")
+  console.log("User clicked on the submit button")
+  alert("Your Request has been sent!")
 });
 
