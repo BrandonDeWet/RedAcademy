012c78ce00998317ff8b64232c3e167257fcad11
Brandon De Wet <brandond@redpandasoftware.co.za>
2026-07-27 16:27:27 +0200

    Updated the HTML page, made it neater added spacing, Added Variables and function to the JS page which will gather information put into the form. Added screenshot of the page to the JS validation docx

--- PM-04/pm04-final-project/script.js
+++ PM-04/pm04-final-project/script.js
@@ -1,3 +1,30 @@
+// Adding an array
+let requests = [];
+
+// Adding const variables based off our HTML page Note, The ID's are based off the id's for the labels in the HTML
+const form = document.getElementById("requestForm");
+const requesterName = document.getElementById("requesterName");
+const department = document.getElementById("department");
+const resourceType = document.getElementById("resourceType");
+const details = document.getElementById("details");
+const urgency = document.getElementById("priority");
+
+// Function for adding an event listener to our form
+form.addEventListener("submit",function(event){
+  // Add the below to prevent the page from refreshing
+  event.preventDefault();
+  // Object creation for containing all request data
+  const request = {
+    requester: requesterName.value,
+    department: department.value,
+    resource: resourceType.value,
+    details: details.value,
+    priority: priority.value
+  };
+  // Pushes data into the array for the request variable above
+  requests.push(request);
+});
+
 // Logs out to the Console when JS gets loaded via the HTML page
 console.log("JavaScript file loaded.");
 
