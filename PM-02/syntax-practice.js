10882855ef9fdcac0dfc79f71c6d081369b0d045
Brandon De Wet <brandond@redpandasoftware.co.za>
2026-09-01 08:42:07 +0200

    Working on KM02 mini activities and evidence

---
+++ PM-02/syntax-practice.js
@@ -0,0 +1,46 @@
+//NOTES:
+// Single line comment
+/* multi line comment */
+
+// Topic 1 Mini Activity
+/*let learnerName = "Brandon De Wet";
+let department = "DevOps";
+let openTickets = "In Progress";
+
+console.log("Hi " +learnerName+ ", You are currently in the " +department+ " department and have tickets " +openTickets+".");*/
+
+// Topic 2 Mini Activity
+/*const requestId = 215;
+const itemRequested = "Lounge Suite";
+let quantity = 1;
+let isApproved = true;
+
+console.log("Hi valued customer your purchase of " +quantity+ ", " +itemRequested+ " has been approved status is: " +isApproved+ ". Your reference number is: " +requestId+ ".");*/
+
+// Topic 3 Mini Activity
+/*let fullName = "Brandon De Wet";
+let age = 28;
+let hasLaptop = true;
+let completeModules = ["JavaScript", "Azure", "Docker", "Kubernetes", "MS-SQL"];
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof hasLaptop);
+console.log(typeof completeModules);*/
+
+// Topic 4 Mini Activity
+/*const storeItemPrice = 15.50;
+let quantity = 2;
+let discount = 0.10;
+const vatRate = 0.15;
+const subTotal = storeItemPrice * quantity;
+const discountedSubtotal = subTotal * discount;
+const vatAmount = subTotal * vatRate;
+const finalAmount = subTotal - discountedSubtotal + vatAmount;
+
+console.log("SubTotal: R"+subTotal+"");
+console.log("Total Discount: R"+discountedSubtotal+"");
+console.log("Total Vat Amount R"+vatAmount+"");
+console.log("Final Total Amount R"+finalAmount+"");*/
+
+// Topic 5 Mini Activity
